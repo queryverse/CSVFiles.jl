671e7949068373e591b3a7be0541e01225c09ff6
David Anthoff <anthoff@berkeley.edu>
2017-06-08 12:01:57 -0700

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
-using CSVFiles
+using FileIO
+using IterableTables
+using DataFrames
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "CSVFiles" begin
+
+df = load(joinpath(@__DIR__, "data.csv")) |> DataFrame
+@test size(df) == (3,3)
+@test df[:Name] == ["John", "Sally", "Jim"]
+@test df[:Age] == [34.,54.,23]
+@test df[:Children] == [2,1,0]
+
+output_filename = tempname()
+
+df |> save(output_filename)
+
+df2 = load(output_filename) |> DataFrame
+
+@test df == df2
+
+end
