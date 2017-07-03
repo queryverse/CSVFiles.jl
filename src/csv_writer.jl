45f19fd99f8f51dbdedede3ca69ad0cade3e3896
David Anthoff <anthoff@berkeley.edu>
2017-07-03 12:44:17 +0200

    Update various option names

--- src/csv_writer.jl
+++ src/csv_writer.jl
@@ -1,38 +1,38 @@
-function _writevalue(io::IO, value::String, delim, quote_char, escape_char)
-    if isnull(quote_char)
+function _writevalue(io::IO, value::String, delim, quotechar, escapechar)
+    if isnull(quotechar)
         print(io, value)
     else
-        quote_char_unpacked = get(quote_char)
-        print(io, quote_char_unpacked)
+        quotechar_unpacked = get(quotechar)
+        print(io, quotechar_unpacked)
         for c in value
-            if c==quote_char_unpacked
-                print(io, escape_char)
+            if c==quotechar_unpacked
+                print(io, escapechar)
             end
             print(io, c)
         end
-        print(io, quote_char_unpacked)
+        print(io, quotechar_unpacked)
     end
 end
 
-function _writevalue(io::IO, value, delim, quote_char, escape_char)
+function _writevalue(io::IO, value, delim, quotechar, escapechar)
     print(io, value)
 end
 
-function _writevalue{T}(io::IO, value::DataValue{T}, delim, quote_char, escape_char)
+function _writevalue{T}(io::IO, value::DataValue{T}, delim, quotechar, escapechar)
     if isnull(value)
         print(io, "NA")
     else
-        _writevalue(io, get(value), delim, quote_char, escape_char)
+        _writevalue(io, get(value), delim, quotechar, escapechar)
     end
 end
 
 
-@generated function _writecsv{T}(io::IO, it, ::Type{T}, delim, quote_char, escape_char)
+@generated function _writecsv{T}(io::IO, it, ::Type{T}, delim, quotechar, escapechar)
     col_names = fieldnames(T)
     n = length(col_names)
     push_exprs = Expr(:block)
     for i in 1:n
-        push!(push_exprs.args, :( _writevalue(io, i.$(col_names[i]), delim, quote_char, escape_char) ))
+        push!(push_exprs.args, :( _writevalue(io, i.$(col_names[i]), delim, quotechar, escapechar) ))
         if i<n
             push!(push_exprs.args, :( print(io, delim ) ))
         end
@@ -46,23 +46,23 @@ end
     end
 end
 
-function save(f::FileIO.File{FileIO.format"CSV"}, data; delim=',', quote_char='"', escape_char='\\', header=true)
+function save(f::FileIO.File{FileIO.format"CSV"}, data; delim=',', quotechar='"', escapechar='\\', header=true)
     isiterabletable(data) || error("Can't write this data to a CSV file.")
 
     it = getiterator(data)
     colnames = IterableTables.column_names(it)
 
-    quote_char_internal = quote_char==nothing ? Nullable{Char}() : Nullable{Char}(quote_char)
+    quotechar_internal = quotechar==nothing ? Nullable{Char}() : Nullable{Char}(quotechar)
 
     open(f.filename, "w") do io
         if header
-            if isnull(quote_char_internal)
+            if isnull(quotechar_internal)
                 join(io,[string(colname) for colname in colnames],delim)
             else
-                join(io,["$(quote_char)" *replace(string(colname), quote_char, "$(escape_char)$(quote_char)") * "$(quote_char)" for colname in colnames],delim)
+                join(io,["$(quotechar)" *replace(string(colname), quotechar, "$(escapechar)$(quotechar)") * "$(quotechar)" for colname in colnames],delim)
             end
             println(io)
         end
-        _writecsv(io, it, eltype(it), delim, quote_char_internal, escape_char)
+        _writecsv(io, it, eltype(it), delim, quotechar_internal, escapechar)
     end    
 end
