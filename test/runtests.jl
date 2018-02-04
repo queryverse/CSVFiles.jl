ab29eaf74bd193922db755bde2caf22aa6d776e8
David Anthoff <anthoff@berkeley.edu>
2018-02-03 19:56:06 -0800

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,6 +27,7 @@ end
 csvf = load(joinpath(@__DIR__, "data.csv"))
 
 @test isiterable(csvf) == true
+@test TableTraits.isiterabletable(csvf) == true
 
 array3 = [@NT(a=DataValue(3),b="df\"e"),@NT(a=DataValue{Int}(),b="something")]
 
@@ -41,5 +42,29 @@ end
 array = collect(load("https://raw.githubusercontent.com/davidanthoff/CSVFiles.jl/v0.2.0/test/data.csv"))
 @test length(array) == 3
 @test array == [@NT(Name="John",Age=34.,Children=2),@NT(Name="Sally",Age=54.,Children=1),@NT(Name="Jim",Age=23.,Children=0)]
+
+output_filename3 = tempname() * ".tsv"
+
+try
+    array |> save(output_filename3)
+
+    array4 = collect(load(output_filename3))
+    @test length(array4) == 3
+    @test array4 == array
+finally
+    gc()
+    rm(output_filename3)
+end
+
+output_filename4 = tempname() * ".csv"
+
+try
+    array |> save(output_filename4, quotechar=nothing)
+
+finally
+    gc()
+    rm(output_filename4)
+end
+
 
 end
